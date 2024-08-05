5f61e5101c26a65703123e50cca3f46775da9e6f
vladislav2251 <vladdidyk1985@gmail.com>
2024-08-05 20:28:31 +0300

    last commit

--- spotify-clone/src/views/HomeView.vue
+++ spotify-clone/src/views/HomeView.vue
@@ -1,8 +1,61 @@
 <template>
-    <div class="">
-      
+    <div class="p-8">
+      <button
+        type="button"
+        class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+      >
+        Podcasts to make you smarter
+      </button>
+
+      <div class="py-1.5"></div>
+
+      <div class="flex items-center">
+        <HomeCard image="https://picsum.photos/id/30/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard image="https://picsum.photos/id/55/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="md:block hidden" image="https://picsum.photos/id/20/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="lg:block hidden" image="https://picsum.photos/id/50/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="xl:block hidden" image="https://picsum.photos/id/25/300/300" title="Title is here" subTitle="Subtitle is here" />
+      </div>
+    </div>
+
+    <div class="p-8">
+      <button
+        type="button"
+        class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+      >
+        Recommended radio
+      </button>
+
+      <div class="py-1.5"></div>
+
+      <div class="flex items-center">
+        <HomeCard image="https://picsum.photos/id/60/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard image="https://picsum.photos/id/98/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="md:block hidden" image="https://picsum.photos/id/73/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="lg:block hidden" image="https://picsum.photos/id/27/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="xl:block hidden" image="https://picsum.photos/id/99/300/300" title="Title is here" subTitle="Subtitle is here" />
+      </div>
+    </div>
+
+    <div class="p-8">
+      <button
+        type="button"
+        class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+      >
+        Recommended songs
+      </button>
+
+      <div class="py-1.5"></div>
+
+      <div class="flex items-center">
+        <HomeCard image="https://picsum.photos/id/33/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard image="https://picsum.photos/id/44/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="md:block hidden" image="https://picsum.photos/id/61/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="lg:block hidden" image="https://picsum.photos/id/11/300/300" title="Title is here" subTitle="Subtitle is here" />
+        <HomeCard class="xl:block hidden" image="https://picsum.photos/id/1/300/300" title="Title is here" subTitle="Subtitle is here" />
+      </div>
     </div>
   </template>
   <script setup>
-  
+  import HomeCard from '@/components/HomeCard.vue';
   </script>
